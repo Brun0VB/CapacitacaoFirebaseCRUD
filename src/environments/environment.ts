27fd5935fcf23fc676d740168efec5dabc6d2d43
Bruno Vilela <brunovilela2002@hotmail.com>
2023-03-14 00:15:21 -0300

    alterações de html css

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,15 @@
 
 export const environment = {
   production: false,
-  //firebase conta BancosDev
+  //firebase conta bruno
   firebase: {
-    apiKey: "AIzaSyDeXzlL4zUc4FJgq94oIO4BnLnuVpj93b8",
-    authDomain: "tutorial-e3acc.firebaseapp.com",
-    projectId: "tutorial-e3acc",
-    storageBucket: "tutorial-e3acc.appspot.com",
-    messagingSenderId: "869336423075",
-    appId: "1:869336423075:web:cf9b639680b06246c97d16",
-    measurementId: "G-YZZKVBDTFL"
+    apiKey: "AIzaSyAF_19f6e4UR3cemdXQXFVO8bZ10s86mKA",
+    authDomain: "capacitacaofirebasecrud.firebaseapp.com",
+    projectId: "capacitacaofirebasecrud",
+    storageBucket: "capacitacaofirebasecrud.appspot.com",
+    messagingSenderId: "656662940018",
+    appId: "1:656662940018:web:9dc8fee4cf1fc256df75eb",
+    measurementId: "G-EK9JSSLGVV"  
   },
 };
 
@@ -25,4 +25,10 @@ export const environment = {
     measurementId: 'G-MQ6R72D1MX',
   },
   */
-
+// apiKey: "AIzaSyDeXzlL4zUc4FJgq94oIO4BnLnuVpj93b8",
+//     authDomain: "tutorial-e3acc.firebaseapp.com",
+//     projectId: "tutorial-e3acc",
+//     storageBucket: "tutorial-e3acc.appspot.com",
+//     messagingSenderId: "869336423075",
+//     appId: "1:869336423075:web:cf9b639680b06246c97d16",
+//     measurementId: "G-YZZKVBDTFL"
